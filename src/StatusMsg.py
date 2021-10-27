bcf02fa722d054d6c96fc92be544bcb331600a3d
hrudhai98 <hrudhai_rajasekhar@krtrimaiq.ai>
2021-10-27 23:41:35 +0530

    Adding Image Extraction for JK, RAWCSV in new format, StateDistricts.csv from incovid19, DistrictMappingMaster.json

--- src/StatusMsg.py
+++ src/StatusMsg.py
@@ -1,28 +1,28 @@
-def StatusMsg(StateCode:str,date:str,StatusCode:str,statusMessage:str,program:str="GetSource") ->None:
+def StatusMsg(StateCode: str, date: str, StatusCode: str, statusMessage: str, program: str = "GetSource") -> None:
     """
     Routine to write the status of Program1(or GetSource) for each state
-    
+
     StateCode: State Code (Ex. Karnataka KA)
     date: Current Date in format YYYY-MM-DD
     program: Name of the calling program
     StatusCode: Whether there is an error or its ok (OK/ERR)
     statusMessage: Text describing the status of the call
-    
+
     """
     if not StateCode:
         print("No State Code!")
-        log_message="Verify Calling program, State Code missing!"
+        log_message = "Verify Calling program, State Code missing!"
     elif not date:
         print("No date!")
-        log_message="Verify Calling program, State Code missing!"
+        log_message = "Verify Calling program, State Code missing!"
     elif not StatusCode:
         print("Status Code missing!")
-        log_message="Verify Calling program, Status Code missing!"
+        log_message = "Verify Calling program, Status Code missing!"
     elif not statusMessage:
         print("No status message!")
-        log_message="Verify Calling program, status message missing!"
-    else:    
-        log_message=StateCode+"|"+date+"|"+program+"|"+statusMessage+"\n"
-    file_name="../RAWCSV/"+StateCode+".log"
-    f=open(file_name,"w+")
+        log_message = "Verify Calling program, status message missing!"
+    else:
+        log_message = StateCode + "|" + date + "|" + program + "|" + statusMessage + "\n"
+    file_name = "../RAWCSV/" + date + "/" + StateCode + ".log"
+    f = open(file_name, "w+")
     f.write(log_message)
