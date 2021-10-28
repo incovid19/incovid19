2b41906e438cfff67b53b5f688155a3dd8dd87ea
Go-SuBha <gopi@semanticwebindia.com>
2021-10-28 11:11:59 +0530

    HTML code changes

--- src/getHTMLData.py
+++ src/getHTMLData.py
@@ -5,6 +5,13 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 import urllib.request
+from urllib.error import HTTPError
+from StatusMsg import StatusMsg
+import re
+from slimit import ast
+from slimit.parser import Parser
+from slimit.visitors import nodevisitor
+import json
 
 def getAPData(path):
     soup = BeautifulSoup(open(path, encoding="utf8"), "html.parser")
@@ -19,10 +26,125 @@ def getAPData(path):
     df_summary["val"]= new[1]
     # Dropping old Name columns
     df_summary.drop(columns =["value","variable"], inplace = True)
-
+    
     df_districts = pd.read_html(str(table))[1]
-    df_districts = df_districts.rename(columns=df_districts.loc[0]).drop(0, axis=0)
+    df_districts.columns = df_districts.iloc[0]
+    print(df_districts.columns)
+    dist_names = {"Name of the District":"District","Confirmed Cases":"Confirmed","Cured/ Discharged":"Recovered","Deceased":"Deceased"}
+    df_districts.rename(columns=dist_names,inplace=True)
+    print(df_districts.columns)
+    df_districts = df_districts[1:-4]
+    
+    df_json = pd.read_json("/mnt/c/Users/gopis/Desktop/django/incovid19/DistrictMappingMaster.json")
+    dist_map = df_json['Andhra Pradesh'].to_dict()
+    df_districts['District'].replace(dist_map,inplace=True)
+
+    print("+"*50)
+    print(df_summary)
+
+    print("="*50)
+    print(df_districts)
     return df_summary,df_districts
+
+def getASData(path):
+    soup = BeautifulSoup(open(path, encoding="utf8"), "html.parser")
+    table = soup.find_all('table')
+    # print(str(table))
+    #reading html file from districts url in sources.csv
+    df_districts = pd.read_html(str(table))[0]
+    #dropping unwanted cols
+    df_districts.drop(columns =["View Details"], inplace = True)
+    #replacing removing - and converting them to NaN(implicit)
+    df_districts.replace({"-":""},inplace =True)
+
+    df_json = pd.read_json("/mnt/c/Users/gopis/Desktop/django/incovid19/DistrictMappingMaster.json")
+    dist_map = df_json['Assam'].to_dict()
+    df_districts['District'].replace(dist_map,inplace=True)
+
+    # converting numeric col to float
+    df_districts = df_districts.apply(pd.to_numeric, errors="ignore")
+    # print(df_districts.sum())
+    #creating dictionary that has summary data for state of Assam
+    dict_temp = {"var":["Confirmed","Active","Recovered","Deceased"],"val":[df_districts.sum()[1],df_districts.sum()[2],df_districts.sum()[3],df_districts.sum()[4]]}
+    df_summary = pd.DataFrame(dict_temp)
+
+    # print("="*50)
+    # print(df_districts)
+    # print("="*50)
+    # print(df_summary)
+    return df_summary,df_districts
+
+def getGJData(path):
+    soup = BeautifulSoup(open(path, encoding="utf8"), "html.parser")
+    table = soup.find_all('table')
+    # print(str(table))
+    #reading html file from districts url in sources.csv
+    df_districts = pd.read_html(str(table))[0]
+    # print(df_districts)
+    #dropping unwanted cols
+    df_districts.drop(columns =["People Under Quarantine"], inplace = True)
+    df_districts = df_districts[:-1]
+    #getting cummulative values
+    df_districts["Cases Tested for COVID19"] = df_districts["Cases Tested for COVID19"].str.split().str[-1]
+    df_districts["Patients Recovered"] = df_districts["Patients Recovered"].str.split().str[-1]
+    
+    #Renaming cols name
+    df_districts.rename(columns={"Active Cases":"Active", "Cases Tested for COVID19": "Tested","Patients Recovered":"Recovered","Total Deaths":"Deceased"},inplace=True)
+    
+    df_json = pd.read_json("/mnt/c/Users/gopis/Desktop/django/incovid19/DistrictMappingMaster.json")
+    dist_map = df_json['Gujarat'].to_dict()
+    df_districts['District'].replace(dist_map,inplace=True)
+
+    df_districts = df_districts.apply(pd.to_numeric, errors="ignore")
+    # print(df_districts.sum())
+    #creating dictionary that has summary data for state of Assam
+    dict_temp = {"var":["Tested","Active","Recovered","Deceased"],"val":[df_districts.sum()[2],df_districts.sum()[1],df_districts.sum()[3],df_districts.sum()[4]]}
+    df_summary = pd.DataFrame(dict_temp)
+
+    print("="*50)
+    print(df_districts)
+    print("="*50)
+    print(df_summary)
+    return df_summary,df_districts
+
+def getODData(path):
+    soup = BeautifulSoup(open(path, encoding="utf8"), "html.parser")
+    script = soup.find_all("script")[-1].string.split(";")
+    data = script[40].split("(")[-1].split(")")[0]
+    df_districts = pd.read_json(data)
+    df_districts.rename(columns={"vchDistrictName":"District","intConfirmed":"Confirmed","intActive":"Active","intDeceased":"Deceased","intRecovered":"Recovered"},inplace=True)
+    df_districts.drop(columns=["intId","intDistid","intCategory","intOthDeceased","dtmCreatedOn","dtmReportedOn","intDistType"],inplace=True)
+    df_districts = df_districts[:-1]
+    
+    df_json = pd.read_json("/mnt/c/Users/gopis/Desktop/django/incovid19/DistrictMappingMaster.json")
+    dist_map = df_json['Odisha'].to_dict()
+    df_districts['District'].replace(dist_map,inplace=True)
+    
+    dict_temp = {"var":["Confirmed","Active","Recovered","Deceased"],"val":[df_districts.sum()[1],df_districts.sum()[2],df_districts.sum()[4],df_districts.sum()[3]]}
+    df_summary = pd.DataFrame(dict_temp)
+    print("="*50)
+    print(df_districts)
+    print("="*50)
+    print(df_summary)
+    return df_summary,df_districts
+
+def getTRData(path):
+    soup = BeautifulSoup(open(path, encoding="utf8"), "html.parser")
+    table = soup.find_all('table')
+    df_districts = pd.read_html(str(table),skiprows=0)[0]
+    df_districts.columns =  [i[1] for i in df_districts.columns]
+    df_districts = df_districts.drop(["SN","Total Person Under Surveillance (Cumulative)","No. of Persons Completed Observation Period (14 Days)",
+    "Facility Surveillance","Home Surveillance","Total Persons Under Surveillance","Sample Negative","Patient Went Out of State"],axis=1).drop(8,axis=0)
+    df_districts.rename(columns= {"Sample Collected & Tested":"Tested","Sample Positive":"Confirmed",
+    "Patient Recovered":"Recovered","Death":"Deceased"}, inplace = True)
+    dict_temp = {"var":["Confirmed","Tested","Recovered","Deceased"],"val":[df_districts.sum()[2],df_districts.sum()[1],df_districts.sum()[3],df_districts.sum()[4]]}
+    df_summary = pd.DataFrame(dict_temp)
+    print("="*50)
+    print(df_districts)
+    print("="*50)
+    print(df_summary)
+    return df_summary,df_districts
+
 
 def getINDData():
     URL = "https://www.mygov.in/corona-data/covid19-statewise-status/"
@@ -103,11 +225,54 @@ def getINDData():
     states_data = states_data.reindex(columns=final_df_col)
     return states_data
 
-def ExtractFromHTML(StateCode = "AP",Date = "2021-10-24"):
-    filepath = "INPUT\\" + Date + "\\" + StateCode + ".html"
-    if StateCode == "AP":
-        return getAPData(filepath)
-    elif StateCode == "IND":
-        return getINDData
+def GenerateRawCsv(StateCode,Date,df_districts):
+    df = pd.DataFrame(columns=["Date","State/UTCode","District","tested_last_updated_district","tested_source_district","notesForDistrict",
+    "cumulativeConfirmedNumberForDistrict","cumulativeDeceasedNumberForDistrict","cumulativeRecoveredNumberForDistrict",
+    "cumulativeTestedNumberForDistrict","last_updated","tested_last_updated_statetested_source_state","notesForState",
+    "cumulativeConfirmedNumberForState","cumulativeDeceasedNumberForState","cumulativeRecoveredNumberForState","cumulativeTestedNumberForState"])
+
+    df['District'] = df_districts['District']
+    if "Confirmed" in df_districts.columns:
+        df['cumulativeConfirmedNumberForDistrict'] = df_districts['Confirmed']
+        df['cumulativeConfirmedNumberForState'] = df['cumulativeConfirmedNumberForDistrict'].sum()
+    if "Tested" in df_districts.columns:
+        df['cumulativeTestedNumberForDistrict'] = df_districts['Tested']
+        df['cumulativeTestedNumberForState'] = df['cumulativeTestedNumberForDistrict'].sum()
+    df['cumulativeDeceasedNumberForDistrict'] = df_districts['Deceased']
+    df['cumulativeRecoveredNumberForDistrict'] = df_districts['Recovered']
+
+    df['Date'] = Date
+    df['State/UTCode'] = StateCode
+     
+    df['cumulativeRecoveredNumberForState'] = df['cumulativeRecoveredNumberForDistrict'].sum()
+    df['cumulativeDeceasedNumberForState'] = df['cumulativeDeceasedNumberForDistrict'].sum()
+
+    df.to_csv("/mnt/c/Users/gopis/Desktop/django/pdf-incovid19/incovid19/RAWCSV/{}/{}_raw.csv".format(Date,StateCode))
+
+def ExtractFromHTML(StateCode = "AS",Date = "2021-10-26"):
+    try:
+        filepath = "/mnt/c/users/gopis/desktop/django/pdf-incovid19/incovid19/INPUT/{0}/{1}.html".format(Date,StateCode)
+        if StateCode == "AP":
+            df_summary,df_districts = getAPData(filepath)
+            GenerateRawCsv(StateCode,Date,df_districts)
+        elif StateCode == "AS":
+            df_summary,df_districts = getASData(filepath)
+            GenerateRawCsv(StateCode,Date,df_districts)
+        elif StateCode == "GJ":
+            df_summary,df_districts = getGJData(filepath)
+            GenerateRawCsv(StateCode,Date,df_districts)
+        elif StateCode == "OD":
+            df_summary,df_districts = getODData(filepath)
+            GenerateRawCsv(StateCode,Date,df_districts)
+        elif StateCode == "TR":
+            df_summary,df_districts = getTRData(filepath)
+            GenerateRawCsv(StateCode,Date,df_districts)
+        elif StateCode == "IND":
+            return getINDData
+        StatusMsg(StateCode,Date,"OK","COMPLETED","ExtractFromHTML")
+    except HTTPError:
+        StatusMsg(StateCode,Date,"ERR","Source URL Not Accessible/ has been changed","ExtractFromHTML")
+    except Exception:
+        StatusMsg(StateCode,Date,"ERR","Fatal error in main loop","ExtractFromHTML")
 
 # dfsummary,dfdistricts = ExtractFromHTML(StateCode = "AP",Date = "2021-10-24")
--- src/getHTMLSources.py
+++ src/getHTMLSources.py
@@ -1,66 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
 import requests
 import pandas as pd
 import ssl
 from shutil import copyfile
 import datetime
+import logging
 from urllib.error import HTTPError
 import urllib
-import timedelta
 from StatusMsg import StatusMsg
-import inflect
 
+if __name__ == "__main__":
+    logging.basicConfig(filename='fetchData.log', 
+                        level=logging.INFO,
+                        format='%(asctime)s %(levelname)-8s %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
 
-p = inflect.engine()
-ssl._create_default_https_context = ssl._create_unverified_context
+    ssl._create_default_https_context = ssl._create_unverified_context
 
-source = pd.read_csv(r"../sources.csv")
+    source = pd.read_csv(r"../sources.csv")
+    today = str(datetime.datetime.now().date())
 
-def downloadFile(Date,StateCode,url):
-    response = requests.get(url)
-    print(url)
-    if response.status_code == 200:
-        StatusMsg(StateCode,Date,"OK","File Downloaded",program="GetSource")
-        with open(r"../INPUT/" + Date + "/" + source["StateCode"][idx] + ".pdf", 'wb') as f:
-            f.write(response.content)
-    else:
-        print("Error")
-        StatusMsg(StateCode,Date,"ERR","File Not Found",program="GetSource")
-
-# today = str(datetime.datetime.now().date())
-today = (datetime.datetime.now() - timedelta.Timedelta(days=1)).date()
-for idx in source.index:
-    if source["StateDataSourceType"][idx] == "html":
-        try:
-            file_name, headers = urllib.request.urlretrieve(source["StateDataURL"][idx])
-            copyfile(file_name, r"../INPUT/" + str(today) + "/" + source["StateCode"][idx] + ".html")
-            StatusMsg(source["StateCode"][idx],str(today),"OK","File Downloaded",program="GetSource")
-        except HTTPError:
-            StatusMsg(source["StateCode"][idx],str(today),"ERR","File Not Found",program="GetSource")
-        except Exception:
-            raise
-            StatusMsg(source["StateCode"][idx],str(today),"ERR","Fatal Error in Main Loop",program="GetSource")
-    elif source["StateDataSourceType"][idx] == "pdf":
-        if source["StateCode"][idx] == "HR":
-            url = 'http://nhmharyana.gov.in/WriteReadData/userfiles/file/CoronaVirus/Daily%20Bulletin%20of%20COVID%2019%20as%20on%20'+str(today.strftime("%d-%m-%Y"))+'.pdf'
-            downloadFile(str(today),source["StateCode"][idx],url)
-        elif source["StateCode"][idx] == "KA":
-            url = 'https://covid19.karnataka.gov.in/storage/pdf-files/EMB-'+str(datetime.datetime.now().date().strftime("%B").upper()[0:3])+'21//'+str(today.strftime("%d-%m-%Y"))+'%20HMB%20English.pdf'
-            downloadFile(str(today),source["StateCode"][idx],url)
-        elif source["StateCode"][idx] == "PB":
-            url = 'https://nhm.punjab.gov.in/advertisements/Media_Bulletin/Media%20Bulletin%20COVID-19%20'+ str(today.strftime("%d-%m-%Y")) +'.pdf'
-            downloadFile(str(today),source["StateCode"][idx],url)
-        elif source["StateCode"][idx] == "TN":
-            url = 'https://stopcorona.tn.gov.in/wp-content/uploads/2020/03/Media-Bulletin-'+str(today.strftime("%d-%m-%y"))+'-COVID-19.pdf'
-            downloadFile(str(today),source["StateCode"][idx],url)
-        elif source["StateCode"][idx] == "UK":
-            url = 'https://health.uk.gov.in/files/'+str(today.strftime("%Y.%m.%d"))+'_Health_Bulletin.pdf'
-            downloadFile(str(today),source["StateCode"][idx],url)
-        elif source["StateCode"][idx] == "WB":
-            url = 'https://www.wbhealth.gov.in/uploaded_files/corona/WB_DHFW_Bulletin_'+p.ordinal(today.day).upper()+'_'+str(datetime.datetime.now().date().strftime("%B").upper()[0:3])+'_REPORT_FINAL.pdf'
-            downloadFile(str(today),source["StateCode"][idx],url)
-        elif source["StateCode"][idx] == "KL":
-            url = 'https://dhs.kerala.gov.in/wp-content/uploads/2021/10/Bulletin-HFWD-English-'+str(datetime.datetime.now().date().strftime("%B"))+'-'+str(today.day)+'.pdf'
-            downloadFile(str(today),source["StateCode"][idx],url)
-        elif source["StateCode"][idx] == "MH":
-            url = 'https://arogya.maharashtra.gov.in/pdf/ncovidmpressnote'+str(datetime.datetime.now().date().strftime("%B")).lower()+str(today.day)+'.pdf'
-            downloadFile(str(today),source["StateCode"][idx],url)
+    for idx in source.index:
+        if source["StateDataSourceType"][idx] == "html":
+            try:
+                file_name, headers = urllib.request.urlretrieve(source["StateDataURL"][idx])
+                copyfile(file_name, r"../INPUT/" + today + "/" + source["StateCode"][idx] + ".html")
+                logging.info("Successfully fetched file for " + source["StateCode"][idx])
+                StatusMsg(source["StateCode"][idx],today,"OK","Completed","GetSource")
+            except HTTPError:
+                StatusMsg(source["StateCode"][idx],today,"ERR","Source URL Not Accessible/ has been changed","GetSource")
+            except Exception:
+                StatusMsg(source["StateCode"][idx],today,"ERR","Fatal error in main loop","GetSource")
