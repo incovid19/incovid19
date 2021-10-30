9b89094c09bf74e02a037b6a62ce0043ca0295da
vishnu prasad <vishnuprasad.vp@yahoo.com>
2021-10-30 19:46:53 +0530

    Git Source Alteration

--- src/main.py
+++ src/main.py
@@ -26,8 +26,8 @@ getSources(source,today)
 print("Extracting Data")
 
 for idx in source.index:
+    print("State:" + source["StateName"][idx])
     if source["StateDataSourceType"][idx] == "Image(Twitter)":
-        print("Image:" + source["StateCode"][idx])
         ExtractDataFromImage(source["StateCode"][idx], str(today), source['Twitter Handle'][idx], source['Twitter Search Term'][idx])
     elif source["StateDataSourceType"][idx] == "html":
         if source["myGov"][idx] != "yes":
