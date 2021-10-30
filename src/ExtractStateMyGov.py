3d984d28e1232dc595d62e7c94483a821b0ed9c5
hrudhai98 <hrudhai_rajasekhar@krtrimaiq.ai>
2021-10-30 18:01:22 +0530

    Adding MyGov

--- src/ExtractStateMyGov.py
+++ src/ExtractStateMyGov.py
@@ -1,9 +1,36 @@
 import pandas as pd
 import json
 import io
+from datetime import datetime, timedelta
+
+# pd.set_option('display.max_rows', None)
+# pd.set_option('display.max_columns', None)
 
 
-def ExtractStateMyGov(state, date):
+# def ExtractNoSource(df, state, date):
+#     date = date - timedelta(1)
+#     # print(df)
+#     state = pd.read_csv(
+#         "../RAWCSV/" + date.strftime("%Y-%m-%d") + "/" + state + "_raw.csv",
+#         index_col=None,
+#         usecols=[
+#             "District",
+#             "cumulativeConfirmedNumberForDistrict",
+#             "cumulativeDeceasedNumberForDistrict",
+#             "cumulativeRecoveredNumberForDistrict",
+#             "last_updated",
+#         ]
+#     )
+#     for district in list(state['District']):
+#         for col in ['']:
+#             pass
+#         # df[]
+#     print(sum(state["cumulativeConfirmedNumberForDistrict"][state['District'] != "Unknown"]))
+#     # print(state)
+#     return df
+
+
+def ExtractStateMyGov(state, date, no_source=False):
     states = json.load(io.open("../StateCode.json"))
     state_name = states[state]
     ind = pd.read_csv(
@@ -45,7 +72,11 @@ def ExtractStateMyGov(state, date):
     state_df["cumulativeDeceasedNumberForState"] = state_df['cumulativeDeceasedNumberForDistrict'][state_df['District'] == 'Unknown'] = int(ind["cumulativeDeceasedNumberForDistrict"][ind["District"].str.contains(state_name)].values[0])
     state_df["cumulativeRecoveredNumberForState"] = state_df['cumulativeRecoveredNumberForDistrict'][state_df['District'] == 'Unknown'] = int(ind["cumulativeRecoveredNumberForDistrict"][ind["District"].str.contains(state_name)].values[0])
     state_df['cumulativeTestedNumberForState'] = None
+
+    # if no_source:
+    #     state_df = ExtractNoSource(state_df, state, datetime.strptime(date, "%Y-%m-%d"))
+
     state_df.to_csv("../RAWCSV/" + date + "/" + state + "_raw.csv", index=False)
 
 
-# ExtractStateMyGov("DN", "2021-10-26")
+# ExtractStateMyGov("SK", "2021-10-29")
