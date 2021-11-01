58fb7f0ad525b0b894f8ac9938f631b7034b6999
Go-SuBha <gopi@semanticwebindia.com>
2021-11-01 23:48:29 +0530

    modified state cummulatives and timeseries fix

--- src/timesseries.py
+++ src/timesseries.py
@@ -20,6 +20,10 @@ def ts_json():
     #     with open(r"..//timeseries_min.json", 'wb') as f:
     #         f.write(response.content)
 
+    with open('../timeSeries/timeseries.min.json') as fp:
+            timeseries_min = json.load(fp)
+            fp.close()
+
     for key in data_min.keys():
         print(key)
         # TS_MIN_dict[key] = {}
@@ -31,9 +35,7 @@ def ts_json():
                 temp_dict[i] = temp[i]
         # temp_dict = {"delta":temp['delta'],"delta7":temp['delta7'],'total':temp['total']}
         # print(temp_dict)
-        with open('../timeSeries/timeseries.min.json') as fp:
-            timeseries_min = json.load(fp)
-            fp.close()
+        
 
         run_date = data_min[key]['meta']['date']
         timeseries_min[key]['dates'][run_date] = temp_dict
