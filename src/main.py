91c51146434d976ed36f054ceb8e8411d778fcf2
Manikanta8899 <vikkurthibannu@gmail.com>
2021-11-03 10:35:27 +0000

    pushing source and extraction files

--- src/main.py
+++ src/main.py
@@ -2,14 +2,14 @@ import os
 import datetime
 import timedelta
 
-today = (datetime.datetime.now() - timedelta.Timedelta(days=1)).date()
-present = datetime.datetime.now().date()
+today = (datetime.datetime.now() - timedelta.Timedelta(days=0)).date()
+# present = datetime.datetime.now().date()
 
 folders = ["INPUT","RAWCSV","LOG"]
 
 for folder in folders:
-    if not os.path.isdir(os.path.join("..",folder,str(present))):
-        os.mkdir(os.path.join("..",folder,str(present)))
+    if not os.path.isdir(os.path.join("..",folder,str(today))):
+        os.mkdir(os.path.join("..",folder,str(today)))
         print("Created")
 
 
@@ -20,9 +20,9 @@ from ExtractStateMyGov import ExtractStateMyGov
 from getHTMLData import ExtractFromHTML
 import pandas as pd
 
-print("getSources for:"+ str(present))
+print("getSources for:"+ str(today))
 source = pd.read_csv(r"../sources.csv")
-getSources(source,present)
+getSources(source,today)
 
 #*********************************************************************
 
@@ -34,7 +34,7 @@ for idx in source.index:
     elif source["StateDataSourceType"][idx] in ["html","json"]:
         if source["myGov"][idx] != "yes":
             fileStatus = os.path.isfile(os.path.join("../INPUT",str(today),source["StateCode"][idx]+ "." + source["StateDataSourceType"][idx]))
-            if not(fileStatus):
+            if not(fileStatus):# and source["StateCode"][idx] != "TT":
                 ExtractStateMyGov(source["StateCode"][idx],str(today),no_source= not(fileStatus))
             else:
                 ExtractFromHTML(source["StateCode"][idx],str(today))
