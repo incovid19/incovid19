2a875ea5bdd748990d8ce280f5ce34d5fbb1dff6
hrudhai98 <hrudhai_rajasekhar@krtrimaiq.ai>
2021-11-08 19:30:48 +0530

    Extracting testing data for AP, OR

--- src/getHTMLData.py
+++ src/getHTMLData.py
@@ -13,6 +13,7 @@ def andhra_pradesh(state, date, path):
     soup = BeautifulSoup(open(path.format(date, state), encoding="utf8"), "html.parser")
 
     table = soup.find_all('table')
+    tested = int(soup.find_all('span', {'id': 'lblSamples'})[0].getText())
     df_summary = pd.read_html(str(table))[0]
     df_summary = df_summary.melt().dropna()
     # new data frame with split value columns
@@ -38,6 +39,7 @@ def andhra_pradesh(state, date, path):
     df['StateConfirmed'] = df_state['Confirmed'] #int(df_summary['val'][df_summary['var'] == 'Confirmed Cases'])
     df['StateRecovered'] = df_state['Recovered'] #int(df_summary['val'][df_summary['var'] == 'Cured/ Discharged'])
     df['StateDeceased'] = df_state['Deceased'] #int(df_summary['val'][df_summary['var'] == 'Deceased'])
+    df['StateTested'] = tested #int(df_summary['val'][df_summary['var'] == 'Deceased'])
     new_dict = {}
     for key, val in df.to_dict().items():
         new_dict[key] = list(val.values())
@@ -126,6 +128,7 @@ def gujarat(state, date, path):
 
 def odisha(state, date, path):
     soup = BeautifulSoup(open(path.format(date, state), encoding="utf8"), "html.parser")
+    tested = soup.find_all('div', {'id': 'odishatab'})[0].find_all('h5')[0].getText().replace(" ", "").split("[")[0].replace(",", "")
     script = soup.find_all("script")[-1].string.split(";")
     for sc in script:
         if "var result" in sc:
@@ -153,6 +156,7 @@ def odisha(state, date, path):
     df['StateConfirmed'] = int(confirmed)
     df['StateRecovered'] = int(recovered)
     df['StateDeceased'] = int(death)
+    df['StateTested'] = int(tested)
 
     dict_temp = {"var": ["Confirmed", "Recovered", "Deceased"],
                  "val": [confirmed, recovered, death]}
@@ -383,7 +387,6 @@ def GenerateRawCsv(state, date, df_districts):
 
     df['cumulativeRecoveredNumberForDistrict'] = df_districts['Recovered']
     df['cumulativeRecoveredNumberForState'] = df_districts['StateRecovered']
-
     df.to_csv("../RAWCSV/{}/{}_raw.csv".format(date, state), index=False)
 
 
@@ -406,4 +409,4 @@ def ExtractFromHTML(state, date):
         ExtractStateMyGov(state, date, no_source=True)
 
 
-ExtractFromHTML(state="TR", date="2021-11-07")
+# ExtractFromHTML(state="OR", date="2021-11-07")
