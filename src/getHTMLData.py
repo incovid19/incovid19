e9ddd11d8dcc504f14abccb809737a4b2f30e9a1
Manikanta8899 <vikkurthibannu@gmail.com>
2021-11-10 15:28:39 +0000

    Updating 10-11-2021 @ 9:00 PM

--- src/getHTMLData.py
+++ src/getHTMLData.py
@@ -424,4 +424,4 @@ def ExtractFromHTML(state, date):
         ExtractStateMyGov(state, date, no_source=True)
 
 
-# ExtractFromHTML(state="AP", date="2021-11-09")
+ExtractFromHTML(state="OR", date="2021-11-09")
