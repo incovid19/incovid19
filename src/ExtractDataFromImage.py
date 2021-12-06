98bbbffafd92dccfeb0db4d33c2b53046bc421c7
hrudhai98 <hrudhai_rajasekhar@krtrimaiq.ai>
2021-12-06 09:21:35 +0530

    Bug fixes to Image Extraction. Added fallback URL for ML pdf. Updated District Mapper

--- src/ExtractDataFromImage.py
+++ src/ExtractDataFromImage.py
@@ -717,7 +717,7 @@ def manipur(state, date, query):
     x = 75
     x2 = 165
     for i, text in enumerate(page[:-1]):
-        if (text.description == "tested") and (text.bounding_poly.vertices[0].x > 400) and (text.bounding_poly.vertices[0].y < y2):
+        if (text.description == "tested") and (text.bounding_poly.vertices[0].x > 350) and (text.bounding_poly.vertices[0].y < y2):
             test_x1, test_x2 = text.bounding_poly.vertices[0].x - 25, text.bounding_poly.vertices[1].x + 25
         if (text.description == "positives") and (text.bounding_poly.vertices[0].x > 400) and (text.bounding_poly.vertices[0].y < y2):
             total_x1, total_x2 = text.bounding_poly.vertices[0].x - 20, text.bounding_poly.vertices[1].x + 15
@@ -1108,7 +1108,7 @@ def ExtractDataFromImage(state, date, handle, term):
 # ExtractDataFromImage('BR', '2021-11-30', 'BiharHealthDept', '#COVIDー19 Updates Bihar')
 # ExtractDataFromImage('CT', '2021-12-01', 'HealthCgGov', '#ChhattisgarhFightsCorona')
 # ExtractDataFromImage('HP', '2021-12-01', 'nhm_hp', '#7PMupdate')
-# ExtractDataFromImage('MN', '2021-12-01', 'health_manipur', 'Manipur updates')
+# ExtractDataFromImage('MN', '2021-12-04', 'health_manipur', 'Manipur updates')
 # ExtractDataFromImage('RJ', '2021-10-27', 'dineshkumawat', '#Rajasthan Bulletin')
 # ExtractDataFromImage('JK', '2021-11-14', 'diprjk', 'Media Bulletin')
 
