eb3702c99cc3f153040aed1a1cedbb88a8703f96
SWIUser1 <asha@semanticwebindia.com>
2021-12-26 12:10:03 +0530

    Update as on 2021-12-26 1200 noon

--- src/ExtractDataFromPDF.py
+++ src/ExtractDataFromPDF.py
@@ -567,7 +567,7 @@ def ExtractFromPDF(StateCode = "KA",Date = "2021-11-22"):
     except Exception:
         StatusMsg(StateCode,Date,"ERR","Fatal error in main loop","ExtractFromPDF")
         
-# ExtractFromPDF(StateCode = "LA",Date = "2021-12-24")
+# ExtractFromPDF(StateCode = "TN",Date = "2021-12-25")
 # ExtractFromPDF(StateCode = "TN",Date = "2021-12-24")
 # ExtractFromPDF(StateCode = "LA",Date = "2021-12-23")
 # ExtractFromPDF(StateCode = "UT",Date = "2021-12-21")
