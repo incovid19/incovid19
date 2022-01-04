8bfbe3ca48ae97ce9a9ad917028673acf8ab603c
SWIUser1 <asha@semanticwebindia.com>
2022-01-04 13:14:35 +0530

    Update as on 2022-01-04 1300

--- src/getHTMLData.py
+++ src/getHTMLData.py
@@ -432,8 +432,8 @@ def ExtractFromHTML(state, date):
         StatusMsg(state, date,"ERR", "Source URL Not Accessible/ has been changed", "ExtractFromHTML")
         # ExtractStateMyGov(state, date, no_source=True)
 
-# ExtractFromHTML(state="TR", date="2022-01-02")
-# ExtractFromHTML(state="MH", date="2022-01-02")
+# ExtractFromHTML(state="TR", date="2022-01-03")
+# ExtractFromHTML(state="MH", date="2022-01-03")
 # ExtractFromHTML(state="KL", date="2021-12-31")
 # ExtractFromHTML(state="GJ", date="2021-10-25")
 # ExtractFromHTML(state="OR", date="2021-10-25")
