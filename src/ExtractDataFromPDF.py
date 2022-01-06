9279beb9711edad2e09964e109f73ffad4eec24d
SWIUser1 <asha@semanticwebindia.com>
2022-01-06 12:28:29 +0530

    Update as on 2022-01-06 1230

--- src/ExtractDataFromPDF.py
+++ src/ExtractDataFromPDF.py
@@ -589,8 +589,8 @@ def ExtractFromPDF(StateCode = "KA",Date = "2021-11-22"):
         
 # for date in dateList:
 #     ExtractFromPDF(StateCode = "PB",Date = str(date))
-# ExtractFromPDF(StateCode = "ML",Date = "2022-01-02")
-# ExtractFromPDF(StateCode = "UT",Date = "2022-01-03")
+# ExtractFromPDF(StateCode = "ML",Date = "2022-01-05")
+# ExtractFromPDF(StateCode = "LA",Date = "2022-01-05")
 # ExtractFromPDF(StateCode = "ML",Date = "2021-12-30")
 # ExtractFromPDF(StateCode = "TN",Date = "2021-10-28")
 # ExtractFromPDF(StateCode = "TN",Date = "2021-10-27")
