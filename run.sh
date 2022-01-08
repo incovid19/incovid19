3a0bd8d8862dea1afc1b659ea66b986bf85e9fae
Akshay Kanthi <aksanoble@gmail.com>
2022-01-08 17:15:34 +0530

    test environment

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cp -a ./. /data/incovid19/
-cd /data/incovid19/ 
-jupyter lab --allow-root --ip 0.0.0.0 --NotebookApp.password=$NOTEBOOK_PASS
+cd /data/incovid19/
+jupyter lab --allow-root --ip 0.0.0.0 --ServerApp.token=$NOTEBOOK_PASS
