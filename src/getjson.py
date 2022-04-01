a76b520da57eb71f65b46a6a9cd44af2abb02cf3
SWIUser1 <asha@semanticwebindia.com>
2022-04-01 13:56:27 +0550

    Update Until2022-04-01 13:36:22.820087

--- src/getjson.py
+++ src/getjson.py
@@ -378,8 +378,8 @@ def date_range(start, end):
 if __name__ == "__main__":
     print("Running Main")
     path = "Test/"
-    start_date = "2022-02-01"
-    end_date = "2022-03-30"
+    start_date = "2022-03-28"
+    end_date = "2022-03-29"
     end = datetime.strptime(end_date, '%Y-%m-%d')
     start = datetime.strptime(start_date, '%Y-%m-%d')
     dateList = date_range(start, end)
