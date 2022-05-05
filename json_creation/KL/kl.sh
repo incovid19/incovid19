08947ada923b030a911b342631197642db51496c
SWIUser1 <asha@semanticwebindia.com>
2022-05-05 12:46:55 +0550

    Update Until2022-05-05 12:26:53.682336

---
+++ json_creation/KL/kl.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+curl -c cookies.txt \
+     -o kerala-index.html https://dashboard.kerala.gov.in/covid/index.php
+
+curl --referer "https://dashboard.kerala.gov.in/covid/index.php" \
+     -b cookies.txt \
+     -c new_cookies.txt \
+     -o kerala-districts.html https://dashboard.kerala.gov.in/covid/dailyreporting-view-public-districtwise.php
+
+for CODE in {1601..1614}; do
+    echo "Downloading for district=$CODE";
+    mv new_cookies.txt cookies.txt
+    curl -X POST \
+	 --referer "https://dashboard.kerala.gov.in/covid/dailyreporting-view-public-districtwise.php" \
+	 -b cookies.txt \
+	 -c new_cookies.txt \
+	 -F district=${CODE} \
+	 -o "kerala-${CODE}.html" https://dashboard.kerala.gov.in/covid/dailyreporting-view-public-districtwise.php
+done
