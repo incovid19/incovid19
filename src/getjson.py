e967259f88f994fd847b24d10aef5b3d4c1ff744
SWIUser1 <asha@semanticwebindia.com>
2022-06-24 10:44:37 +0530

    MH past data update for 2022-06-22

--- src/getjson.py
+++ src/getjson.py
@@ -379,8 +379,8 @@ if __name__ == "__main__":
     print("Running Main")
     # path = "Test/"  #Staging path
     path = "/home/swiadmin/test/v4/min/"    #production path
-    start_date = "2022-06-21"
-    end_date = "2022-06-21"
+    start_date = "2022-06-22"
+    end_date = "2022-06-22"
     end = datetime.strptime(end_date, '%Y-%m-%d')
     start = datetime.strptime(start_date, '%Y-%m-%d')
     dateList = date_range(start, end)
