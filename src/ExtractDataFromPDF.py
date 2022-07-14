6bf5f9dac6c9e0daf8004945fcbb60f9ae249d74
SWIUser1 <asha@semanticwebindia.com>
2022-07-14 15:42:33 +0550

    Update Until2022-07-14 15:22:31.144884

--- src/ExtractDataFromPDF.py
+++ src/ExtractDataFromPDF.py
@@ -475,17 +475,21 @@ def getWBData(file_path,date,StateCode):
 
 
 def getMHData(file_path,date,StateCode):
-    table = camelot.read_pdf(file_path,'1,2,3')
+    # table = camelot.read_pdf(file_path,'1,2,3')
+    table = camelot.read_pdf(file_path,'2,3')
+
     if not os.path.isdir('../INPUT/{}/{}/'.format(date,StateCode)):
         os.mkdir('../INPUT/{}/{}/'.format(date,StateCode))
     table.export('../INPUT/{}/{}/foo.csv'.format(date,StateCode), f='csv')
     # print(table)
     # table[5].to_excel('foo.xlsx')
-    df_districts_1 = pd.read_csv('../INPUT/{}/{}/foo-page-1-table-1.csv'.format(date,StateCode))
+    # df_districts_1 = pd.read_csv('../INPUT/{}/{}/foo-page-1-table-1.csv'.format(date,StateCode))
     df_districts_2 = pd.read_csv('../INPUT/{}/{}/foo-page-2-table-1.csv'.format(date,StateCode))
     df_districts_3 = pd.read_csv('../INPUT/{}/{}/foo-page-3-table-1.csv'.format(date,StateCode))
 
-    frames = [df_districts_1,df_districts_2,df_districts_3]
+    # frames = [df_districts_1,df_districts_2,df_districts_3]
+    frames = [df_districts_2,df_districts_3]
+
     df_districts = pd.concat(frames,ignore_index=True)
     df_districts.columns = df_districts.columns.str.replace("\n","")
   
@@ -1328,7 +1332,7 @@ def ExtractFromPDF(StateCode = "KA",Date = "2021-11-22"):
 # ExtractFromPDF(StateCode = "AP",Date = "2022-04-29")
 # ExtractFromPDF(StateCode = "AP",Date = "2022-03-18")
 # ExtractFromPDF(StateCode = "KA",Date = "2022-04-13")
-# ExtractFromPDF(StateCode = "MH",Date = "2022-05-11")
+# ExtractFromPDF(StateCode = "MH",Date = "2022-07-13")
 # ExtractFromPDF(StateCode = "ML",Date = "2022-07-07")
 # ExtractFromPDF(StateCode = "RJ",Date = "2022-07-09")
 # ExtractFromPDF(StateCode = "ML",Date = "2022-05-24")
