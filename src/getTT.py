22e352f46f8d76ec3bb070d24df029ee9da795c3
SWIUser1 <asha@semanticwebindia.com>
2022-08-31 02:06:00 +0550

    Update Until2022-08-31 01:45:57.844520

--- src/getTT.py
+++ src/getTT.py
@@ -97,11 +97,11 @@ def india(state,date):
     return states_data
 
 def getTT():
-    today = (datetime.datetime.now() - timedelta.Timedelta(days=0)).date()
-    pDate = (datetime.datetime.now() - timedelta.Timedelta(days=1)).date()
-    cowinDate = (datetime.datetime.now() - timedelta.Timedelta(days=2)).date()
-    TT_df = india("TT",str(today))
-    # TT_df = pd.read_csv("../RAWCSV/2022-08-30/TT_raw.csv")
+    today = (datetime.datetime.now() - timedelta.Timedelta(days=1)).date()
+    pDate = (datetime.datetime.now() - timedelta.Timedelta(days=2)).date()
+    cowinDate = (datetime.datetime.now() - timedelta.Timedelta(days=3)).date()
+    # TT_df = india("TT",str(today))
+    TT_df = pd.read_csv("../RAWCSV/2022-08-30/TT_raw.csv")
 
     if not os.path.isdir(os.path.join("..","RAWCSV",str(pDate),"myGov")):
         os.mkdir(os.path.join("..","RAWCSV",str(pDate),"myGov"))
@@ -171,7 +171,7 @@ def getTT():
 
     TT_df["tested_source_state"] = "https://www.icmr.gov.in/"
 
-    TT_df["tested_last_updated_state"] = str(datetime.datetime.strptime(TT_df["tested_last_updated_state"][0] , '%b %d, %Y'))
+    # TT_df["tested_last_updated_state"] = str(datetime.datetime.strptime(TT_df["tested_last_updated_state"][0] , '%b %d, %Y'))
 
 
     delta_date = str(pDate)
