37a7c5315c1be55ea473401ffd2814f867d4617c
SWIUser1 <asha@semanticwebindia.com>
2022-08-31 19:11:16 +0550

    Update Until2022-08-31 18:51:14.819215

--- src/ExtractDataFromPDF.py
+++ src/ExtractDataFromPDF.py
@@ -1490,7 +1490,7 @@ def ExtractFromPDF(StateCode = "KA",Date = "2021-11-22"):
 #     ExtractFromPDF(StateCode = "NL",Date = str(date.date()))
 
 # ExtractFromPDF(StateCode = "UT",Date = "2022-08-22")
-ExtractFromPDF(StateCode = "RJ",Date = "2022-08-29")
+# ExtractFromPDF(StateCode = "RJ",Date = "2022-08-29")
 # ExtractFromPDF(StateCode = "ML",Date = "2022-08-22")
 # ExtractFromPDF(StateCode = "PB",Date = "2022-08-22")
 # ExtractFromPDF(StateCode = "MH",Date = "2022-08-04")
