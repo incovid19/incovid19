43d5060f54d80c8bb750fc452023c7d5a5152bb2
SWIUser1 <asha@semanticwebindia.com>
2022-09-12 18:01:19 +0530

    PB corrected data

--- src/getjson.py
+++ src/getjson.py
@@ -379,8 +379,8 @@ if __name__ == "__main__":
     print("Running Main")
     # path = "Test/"  #Staging path
     path = "/home/swiadmin/test/v4/min/"    #production path
-    start_date = "2022-09-05"
-    end_date = "2022-09-09"
+    start_date = "2022-09-06"
+    end_date = "2022-09-10"
     end = datetime.strptime(end_date, '%Y-%m-%d')
     start = datetime.strptime(start_date, '%Y-%m-%d')
     dateList = date_range(start, end)
