6f18408b50ee3b735de87fc278fe8498f4f78572
SWIUser1 <asha@semanticwebindia.com>
2023-06-10 04:16:09 +0550

    Update Until2023-06-10 03:56:06.656070

--- src/getTT.py
+++ src/getTT.py
@@ -215,9 +215,9 @@ def india(state,date):
     return states_data
 
 def getTT():
-    today = (datetime.datetime.now() - timedelta.Timedelta(days= 21 + 0)).date()
-    pDate = (datetime.datetime.now() - timedelta.Timedelta(days= 21 + 1)).date()
-    cowinDate = (datetime.datetime.now() - timedelta.Timedelta(days= 21 + 2)).date()
+    today = (datetime.datetime.now() - timedelta.Timedelta(days= 20 + 0)).date()
+    pDate = (datetime.datetime.now() - timedelta.Timedelta(days= 20 + 1)).date()
+    cowinDate = (datetime.datetime.now() - timedelta.Timedelta(days= 20 + 2)).date()
     TT_df = india("TT",str(today))
     # TT_df = pd.read_csv("../RAWCSV/2022-08-30/TT_raw.csv")
 
