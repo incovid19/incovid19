63cc41fff75de101d780c4ff709607d04b194551
SWIUser1 <asha@semanticwebindia.com>
2023-06-27 13:38:08 +0550

    Update Until2023-06-27 13:18:05.672305

--- src/getTT.py
+++ src/getTT.py
@@ -178,7 +178,8 @@ def india(state,date):
         columns={0: "District", 1: "cumulativeConfirmedNumberForDistrict", 2: "cumulativeRecoveredNumberForDistrict",
                  3: "cumulativeDeceasedNumberForDistrict"})
 
-    states_data["Date"] = date#str(datetime.datetime.now().date())
+    states_data["Date"] = date
+    # states_data["Date"] = str(datetime.datetime.now().date())
 
     states_data["State/UTCode"] = state
 
@@ -215,9 +216,9 @@ def india(state,date):
     return states_data
 
 def getTT():
-    today = (datetime.datetime.now() - timedelta.Timedelta(days= 1 + 0)).date()
-    pDate = (datetime.datetime.now() - timedelta.Timedelta(days= 1 + 1)).date()
-    cowinDate = (datetime.datetime.now() - timedelta.Timedelta(days= 1 + 2)).date()
+    today = (datetime.datetime.now() - timedelta.Timedelta(days= 5 + 0)).date()
+    pDate = (datetime.datetime.now() - timedelta.Timedelta(days= 5 + 1)).date()
+    cowinDate = (datetime.datetime.now() - timedelta.Timedelta(days= 5 + 2)).date()
     TT_df = india("TT",str(today))
     # TT_df = pd.read_csv("../RAWCSV/2022-08-30/TT_raw.csv")
 
@@ -251,7 +252,7 @@ def getTT():
                         temp_df = ExtractStateMyGov(source["StateCode"][idx],str(today), no_source = True)
                 temp_df["Date"] = pDate
                 temp_df["notesForState"] = "As of 1st November 2022, this site will reflect the National and State level data as published by MoHFW. The district level data will not be updated beyond 31st October 2022."
-                # temp_df["notesForState"] = "Since there are no updates for 20th June from MoHFW the portal reflects data of 19th June 2023"
+                # temp_df["notesForState"] = "Since there are no updates for 22th June from MoHFW the portal reflects data of 21th June 2023"
                 temp_df.to_csv(os.path.join("..","RAWCSV",str(pDate),"myGov",source["StateCode"][idx]+"_raw.csv"))
     except ValueError:
         print("Tested Values missing for DL/WB for the Date:"+ str(pDate))
