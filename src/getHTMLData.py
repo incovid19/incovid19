9d7e560fbb136b56b3d7a5bfaa13f132d37c35b2
SWIUser1 <asha@semanticwebindia.com>
2023-12-26 15:43:52 +0550

    Update Until2023-12-26 15:23:49.736378

--- src/getHTMLData.py
+++ src/getHTMLData.py
@@ -644,4 +644,5 @@ def ExtractFromHTML(state, date):
 # ExtractFromHTML(state="TT", date="2023-08-15")
 # ExtractFromHTML(state="TT", date="2023-08-16")
 # ExtractFromHTML(state="TT", date="2023-08-17")
-ExtractFromHTML(state="TT", date="2023-08-18")
+# ExtractFromHTML(state="TT", date="2023-08-18")
+ExtractFromHTML(state="TT", date="2023-08-19")
